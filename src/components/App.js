4af07e5f49c71c547ada3fc697cba3ce4394b2a2
Namrata Prakash Nakhale <114344082+namratanakhale@users.noreply.github.com>
2022-11-21 11:15:54 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react'
+import '../styles/App.css';
+const App = () => {
+//code here
+  return (
+    <div id="main">
+      <input id='input' onChange={}></input>
+      <button id='button' onClick={}>Click</button>
+      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+    </div>
+  )
+}
+
+
+export default App;
